9ea87b9a8028a43037da5bee30d78da03ebd2025
Dhruv Pokhriyal <171908255+DhruvPokhriyal@users.noreply.github.com>
2024-12-09 19:52:03 +0530

    Add contact details in about us section
    Add contact details in about us section and also add some extra styling
    to contact details

--- src/aboutus.js
+++ src/aboutus.js
@@ -7,10 +7,13 @@ export function aboutUsSetting() {
     heading.classList.add("about-heading");
     const content = document.createElement("p");
     content.classList.add("about-content");
+    const contactDetails = document.createElement("p");
+    contactDetails.classList.add("contacts");
     heading.textContent = "About";
     content.textContent = `At Fornax, we know the value of a home-cooked meal. Every dish we create is made fresh on-site, with ingredients sourced or harvested on the same day. We believe in slow cooking and meal-savoring - because good things take time.
 Now that times have changed, we must adapt. With our new Cook-It-Yourself (CIY) Meal Kits, we bring the Kitchen Comfort experience straight to your doorstep. Inside, you'll find everything you need to recreate our beloved home-style meals: Easy to follow illustrated recipes, prepared ingredients, and even a QR code for real-time cooking videos to help you unleash your inner chef.
 Times have changed, but our philosophy has not. We believe in the life-changing comfort of a good meal - and now you can cook it yourself!`;
+    contactDetails.textContent = "Number : 1234567890";
     const aboutSection = document.createElement("div");
     aboutSection.classList.add("abt");
     const aboutBg = document.createElement("div");
@@ -23,6 +26,7 @@ Times have changed, but our philosophy has not. We believe in the life-changing 
     imgContainer.classList.add("about-image-container");
     aboutSection.appendChild(heading);
     aboutSection.appendChild(content);
+    aboutSection.appendChild(contactDetails);
     imgContainer.appendChild(aboutImg);
     aboutBg.appendChild(imgContainer);
     aboutBg.appendChild(aboutSection);
