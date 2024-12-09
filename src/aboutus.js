4388b708f70358e71033b10fad159adeb2e5543f
Dhruv Pokhriyal <171908255+DhruvPokhriyal@users.noreply.github.com>
2024-12-09 18:47:06 +0530

    Create a basic structure for the about page
    This commit creates a basic structure for the about page and function to
    allow switch to about section

---
+++ src/aboutus.js
@@ -0,0 +1,19 @@
+import "./aboutus.css";
+
+export function aboutUsSetting() {
+    const main = document.querySelector("#content");
+    main.innerHTML = "";
+    const heading = document.createElement("h1");
+    heading.classList.add("about-heading");
+    const content = document.createElement("p");
+    content.classList.add("about-content");
+    heading.textContent = "About";
+    content.textContent = `At Fornax, we know the value of a home-cooked meal. Every dish we create is made fresh on-site, with ingredients sourced or harvested on the same day. We believe in slow cooking and meal-savoring - because good things take time.
+Now that times have changed, we must adapt. With our new Cook-It-Yourself (CIY) Meal Kits, we bring the Kitchen Comfort experience straight to your doorstep. Inside, you'll find everything you need to recreate our beloved home-style meals: Easy to follow illustrated recipes, prepared ingredients, and even a QR code for real-time cooking videos to help you unleash your inner chef.
+Times have changed, but our philosophy has not. We believe in the life-changing comfort of a good meal - and now you can cook it yourself!`;
+    const aboutSection = document.createElement("div");
+    aboutSection.classList.add("abt");
+    aboutSection.appendChild(heading);
+    aboutSection.appendChild(content);
+    main.append(aboutSection);
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import * as homepage from "./homepage.js";
+import * as aboutpage from "./aboutus.js";
 import "./styles.css";
+
+const homeButton = document.querySelector(".home");
+const aboutButton = document.querySelector(".about");
+const menuButton = document.querySelector(".menu");
 
 window.onload = function () {
     homepage.homepageSetting();
-    const homeButton = document.querySelector(".home");
     homeButton.classList.add("currentPage");
 };
+
+aboutButton.addEventListener("click", () => {
+    homeButton.classList.remove("currentPage");
+    menuButton.classList.remove("currentPage");
+    aboutButton.classList.add("currentPage");
+    aboutpage.aboutUsSetting();
+});
