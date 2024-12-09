889e5cf44c3527c6f3598345c21003309fa92654
Dhruv Pokhriyal <171908255+DhruvPokhriyal@users.noreply.github.com>
2024-12-09 19:15:13 +0530

    Bold the heading of the current section in navbar

--- src/index.js
+++ src/index.js
@@ -3,32 +3,41 @@ import * as homepage from "./homepage.js";
 import * as aboutpage from "./aboutus.js";
 import "./styles.css";
 
+const content = document.querySelector("#content");
 const homeButton = document.querySelector(".home");
 const aboutButton = document.querySelector(".about");
 const menuButton = document.querySelector(".menu");
 
+function transitionContent(newContent) {
+    content.classList.add("hidden");
+    setTimeout(() => {
+        newContent();
+        content.classList.remove("hidden");
+    }, 500);
+}
+
 window.onload = function () {
-    homepage.homepageSetting();
     homeButton.classList.add("currentPage");
+    transitionContent(homepage.homepageSetting);
 };
 
 menuButton.addEventListener("click", () => {
     homeButton.classList.remove("currentPage");
     aboutButton.classList.remove("currentPage");
     menuButton.classList.add("currentPage");
-    menupage.menuSetting();
+    transitionContent(menupage.menuSetting);
 });
 
 homeButton.addEventListener("click", () => {
     aboutButton.classList.remove("currentPage");
     menuButton.classList.remove("currentPage");
     homeButton.classList.add("currentPage");
-    homepage.homepageSetting();
+    transitionContent(homepage.homepageSetting);
 });
 
 aboutButton.addEventListener("click", () => {
     homeButton.classList.remove("currentPage");
     menuButton.classList.remove("currentPage");
     aboutButton.classList.add("currentPage");
-    aboutpage.aboutUsSetting();
+    transitionContent(aboutpage.aboutUsSetting);
 });
